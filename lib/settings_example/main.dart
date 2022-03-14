7229f047e782da4ead155300fb75bdbe91af01fa
Shinya Moriyama <mori.sin.1979@gmail.com>
2022-03-15 05:22:47 +0900

    settings_ui

---
+++ lib/settings_example/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/settings_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Settings UI Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.deepPurple,
+        brightness: Brightness.dark,
+      ),
+      home: SettingsScreen(),
+    );
+  }
+}
